478e1f5a1895220c613e4ad252113cefe40a4b83
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-06-12 14:54:58 -0500

    feat: configured landing page and files

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,6 +8,7 @@ const config: GatsbyConfig = {
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
+  // @ts-ignore
   graphqlTypegen: true,
   plugins: ["gatsby-plugin-postcss",
     // "gatsby-plugin-google-gtag", 
@@ -16,19 +17,26 @@ const config: GatsbyConfig = {
       options: {
         "icon": "src/images/icon.png"
       }
-    }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    },
+    // "gatsby-plugin-mdx", 
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         "name": "images",
         "path": "./src/images/"
       },
+      //@ts-ignore
       __key: "images"
-    }, {
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         "name": "pages",
         "path": "./src/pages/"
       },
+      //@ts-ignore
       __key: "pages"
     }]
 };
