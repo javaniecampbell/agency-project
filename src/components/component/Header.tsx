cc61eed1b284ba7fffaeb6d6b2e235fb02ea94d4
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-06-13 04:42:55 -0500

    refactor: update header component to include "Get Started" button

--- src/components/component/Header.tsx
+++ src/components/component/Header.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
-import { Link } from 'gatsby';
-import { MountainIcon } from './MountainIcon';
+import { Link } from "gatsby";
+import { MountainIcon } from "./MountainIcon";
 
 const Header: React.FC = () => {
   return (
-    <header className="px-4 lg:px-6 h-14 flex items-center">
+    <header className="px-4 lg:px-6 h-14 flex items-center justify-center">
       <Link className="flex items-center justify-center" to="#">
         <MountainIcon className="h-6 w-6" />
         <span className="sr-only">Your Business Agency</span>
@@ -35,6 +35,12 @@ const Header: React.FC = () => {
           Contact
         </Link>
       </nav>
+      <Link
+        className="hidden ml-4 md:inline-flex h-8 items-center justify-center rounded-md bg-gray-900 px-6 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+        to="#contact-us"
+      >
+        Get Started
+      </Link>
     </header>
   );
 };
