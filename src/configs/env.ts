7918225f56e1a88a5263190effab1c3d82f9703e
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-06-15 09:22:08 -0500

    feat: added configs for website

---
+++ src/configs/env.ts
@@ -0,0 +1,8 @@
+export default {
+    // Add your configuration here
+    // Example:
+    // title: "My Website",
+    // description: "This is my website",
+    // keywords: "website, my website",
+    AIID: "_SzqJvJ0NhoLKjWr0DjO5",
+};
