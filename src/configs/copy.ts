f4a83d851dc781515590c7f576192a3b5322d1e8
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-07-20 18:26:35 -0500

    feat: Adds short copy and call to action to services & products

--- src/configs/copy.ts
+++ src/configs/copy.ts
@@ -1,6 +1,11 @@
 export default {
     ourServices: [{
         title: "Business Development",
+        shortCopy: {
+            main: "Unlock business potential with strategic business development. Tailored growth plans to ensure success",
+            alternate: "Unlock your business potential with strategic growth plans tailored to your vision. Partner with us for sustainable success."
+        },
+        cta: "Learn More",
         description: {
             paragraph1: "Unlock your business potential with our strategic business development services at Olympus Consultancy LLC. We believe that growth is driven by informed decisions and strategic planning. Our team of experts conducts comprehensive market analysis to identify opportunities and challenges, ensuring you have a clear understanding of your competitive landscape. We then work closely with you to develop tailored growth plans that align with your vision and objectives, providing you with a roadmap to success.",
             paragraph2: "Our approach to business development is holistic and proactive. We don't just offer advice; we partner with you to implement strategies that drive sustainable growth. From identifying new revenue streams to optimizing your existing operations, our goal is to help you achieve long-term success. Let us guide you every step of the way, transforming potential into performance",
@@ -9,6 +14,11 @@ export default {
     },
     {
         title: "Business Coaching",
+        shortCopy: {
+            main: "Enhance leadership with personalized business coaching. Achieve goals with expert guidance.",
+            alternate: "Enhance your leadership and management skills with personalized coaching. Achieve your goals with expert guidance."
+        },
+        cta: "Discover More",
         description: {
             paragraph1: "Enhance your leadership and management skills with personalized business coaching from Olympus Consultancy LLC. Our business coaching services are designed to provide you with actionable insights and support tailored to your unique needs. Whether you're a seasoned executive or an emerging leader, our coaches bring a wealth of experience and knowledge to help you navigate the complexities of running a business.",
             paragraph2: "Our coaching sessions focus on developing core competencies such as strategic thinking, effective communication, and decision-making. We provide a supportive environment where you can explore new ideas, address challenges, and refine your leadership style. With our expert guidance, you'll be empowered to achieve your personal and professional goals, driving your business forward with confidence.",
@@ -17,6 +27,11 @@ export default {
     },
     {
         title: "Business Process Automation",
+        shortCopy: {
+            main: "Streamline operations and boost efficiency with our automation solutions. Focus on growth, not tasks.",
+            alternate: "Streamline your operations and boost efficiency with tailored automation solutions. Focus on growth, not repetitive tasks."
+        },
+        cta: "See How",
         description: {
             paragraph1: "Streamline your operations and improve efficiency with our business process automation solutions at Olympus Consultancy LLC. We understand that time is money, and our automation services are designed to help you save both. By automating repetitive and time-consuming tasks, you can free up valuable resources and focus on what really matters—growing your business.",
             paragraph2: "Our business process automation solutions are tailored to meet the unique needs of your organization. We work with you to identify areas where automation can have the greatest impact, from data entry and reporting to customer service and supply chain management. By implementing intelligent workflows, we help you reduce errors, increase productivity, and lower operational costs. Experience the benefits of a streamlined, efficient business with our expert automation services.",
@@ -25,6 +40,11 @@ export default {
     },
     {
         title: "Marketing Automation",
+        shortCopy: {
+            main: "Optimize marketing with cutting-edge automation tools. Engage the audience and maximize ROI.",
+            alternate: "Optimize your marketing efforts with cutting-edge automation tools. Engage your audience and maximize ROI."
+        },
+        cta: "Explore Now",
         description: {
             paragraph1: "Boost your marketing efforts with our cutting-edge marketing automation tools at Olympus Consultancy LLC. In today's competitive market, reaching your audience effectively is more important than ever. Our marketing automation solutions are designed to help you do just that, leveraging data-driven strategies to optimize your campaigns and maximize your ROI.",
             paragraph2: "With Olympus Consultancy LLC's marketing automation services, you can create personalized, targeted campaigns that engage your audience at the right time with the right message. Our tools allow you to automate key marketing tasks such as email campaigns, social media posting, and lead nurturing, freeing up your team to focus on strategy and creativity. By using advanced analytics, we provide you with insights into campaign performance, helping you make informed decisions and continually improve your marketing efforts.",
@@ -33,6 +53,11 @@ export default {
     },
     {
         title: "Support Automation",
+        shortCopy: {
+            main: "Deliver exceptional customer support with automated solutions. Improve satisfaction and response times.",
+            alternate: "Deliver exceptional customer support with automated solutions. Improve customer satisfaction and response times."
+        },
+        cta: "Find Out More",
         description: {
             paragraph1: "Provide exceptional customer support with our support automation services at Olympus Consultancy LLC. In an era where customer expectations are higher than ever, timely and efficient support can make all the difference. Our support automation solutions are designed to enhance response times and improve customer satisfaction, allowing you to deliver the high-quality service your customers deserve.",
             paragraph2: "We utilize intelligent automation to streamline your support processes. From automated ticketing systems to AI-driven chatbots, our solutions ensure that customer inquiries are handled swiftly and accurately. By reducing the manual workload on your support team, we enable them to focus on resolving complex issues and building stronger customer relationships. Experience the benefits of faster response times and increased customer loyalty with our support automation services.",
@@ -44,6 +69,11 @@ export default {
     ourProducts: [
         {
             title: "Document Management",
+            shortCopy: {
+                main: "Efficiently manage your documents with our comprehensive system. Secure, accessible, and organized.",
+                alternate: "Efficiently manage documents with our comprehensive system. Secure, accessible, and organized."
+            },
+            cta: "Learn More",
             description: {
                 paragraph1: "Efficiently manage your documents with our comprehensive document management system at Olympus Consultancy LLC. In today's fast-paced business environment, the ability to organize, store, and retrieve files with ease is crucial. Our document management solution provides a centralized platform where all your documents are securely stored and easily accessible. Say goodbye to the clutter and inefficiencies of traditional paper-based systems and embrace the streamlined, digital approach.",
                 paragraph2: "Our document management system is designed to enhance productivity and collaboration. With features such as version control, document sharing, and advanced search capabilities, you can ensure that your team always has access to the most up-to-date information. Additionally, our robust security measures protect your sensitive data, giving you peace of mind. Simplify your document management and focus on what really matters—growing your business.",
@@ -51,6 +81,11 @@ export default {
             image: null,
         }, {
             title: "Market Automation",
+            shortCopy: {
+                main: "Automate marketing campaigns and track performance effortlessly. Drive conversions and engagement.",
+                alternate: "Automate marketing campaigns and track performance effortlessly. Drive conversions and engagement."
+            },
+            cta: " Discover More",
             description: {
                 paragraph1: "Automate your marketing campaigns and track performance with our market automation solutions at Olympus Consultancy LLC. In a world where digital marketing is constantly evolving, staying ahead of the competition requires the right tools. Our market automation platform enables you to engage your audience and drive conversions effortlessly. By automating key marketing tasks, you can focus on strategy and creativity, while our system handles the execution.",
                 paragraph2: "Our market automation solutions provide you with the insights needed to optimize your campaigns. Track performance in real-time, analyze data, and make informed decisions to improve your marketing efforts continuously. From email marketing and social media management to lead nurturing and customer segmentation, our tools help you reach your target audience with precision and efficiency. Boost your marketing effectiveness and achieve higher ROI with our innovative market automation products.",
@@ -58,6 +93,11 @@ export default {
             image: null,
         }, {
             title: "Support Automation",
+            shortCopy: {
+                main: "Enhance customer service with automated support tools. Deliver timely and accurate assistance.",
+                alternate: "Enhance customer service with automated support tools. Deliver timely and accurate assistance."
+            },
+            cta: "See How",
             description: {
                 paragraph1: "Improve your customer service with our support automation tools at Olympus Consultancy LLC. Exceptional customer support is essential for building strong, lasting relationships with your clients. Our support automation solutions are designed to enhance your service capabilities by automating responses, ticket management, and more. With our tools, you can ensure that your customers receive timely and accurate assistance, leading to higher satisfaction and loyalty.",
                 paragraph2: "Our support automation products integrate seamlessly with your existing systems, providing a unified platform for managing customer interactions. Utilize AI-driven chatbots to handle routine inquiries, freeing up your support team to focus on more complex issues. Streamline ticket management with automated workflows, ensuring that no request falls through the cracks. With Olympus Consultancy LLC's support automation tools, you can deliver exceptional service while reducing operational costs and increasing efficiency.",
@@ -65,6 +105,11 @@ export default {
             image: null,
         }, {
             title: "Business Processes",
+            shortCopy: {
+                main: "Optimize operations with tailored business process solutions. Achieve efficiency and productivity.",
+                alternate: "Optimize your operations with tailored business process solutions. Achieve efficiency and productivity."
+            },
+            cta: "Explore Now",
             description: {
                 paragraph1: "Optimize your business processes with our tailored solutions at Olympus Consultancy LLC. Achieving operational excellence requires more than just efficient workflows; it requires solutions that are specifically designed to meet your unique needs. Our business process products help you automate and optimize various aspects of your operations, from workflow automation to process optimization, ensuring that you achieve the highest levels of efficiency and productivity.",
                 paragraph2: "Our approach to business process optimization is comprehensive and collaborative. We work closely with you to understand your specific challenges and goals, then develop customized solutions that address your needs. Whether you're looking to streamline manufacturing processes, improve supply chain management, or enhance administrative functions, our products provide the tools and insights necessary for success. Experience the benefits of optimized business processes and take your operations to the next level with Olympus Consultancy LLC.",
